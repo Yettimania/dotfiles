a8db60bdcf807349f0baf5b643baac6bd14f9868
Kyle Snyder <kylesnyder42@gmail.com>
2023-10-10 08:32:11 -0700

    update visrc config

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -4,6 +4,8 @@ require('plugins/vis-commentary')
 plugin_vis_open = require('plugins/vis-fzf-open')
 plugin_vis_tasks = require('plugins/vis-tasks')
 
+plugin_vis_open.fzf_args = "--height=40%"
+
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
     vis:command('map! normal <C-f> :fzf<Enter>')
@@ -13,6 +15,8 @@ end)
 vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
 	-- Your per window configuration options e.g.
     vis:command('set tw 4')
+    vis:command('set expandtab on')
+    vis:command('set autoindent on')
     vis:command('set rnu on')
     vis:command('set cul on')
     vis:command('set theme gruvbox')
